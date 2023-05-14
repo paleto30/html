70f6962fe7195f77a8eb7287fb3627e720a3ef78
paleto30 <paleto0817@gmail.com>
2023-05-14 09:00:16 -0500

    feat ( first commit ):  :rocket:  explicacion git
    este comit lo hago para la explicacion de git

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi archivo</title>
+</head>
+<body>
+
+<div style="display: flex; justify-content: center;">
+    <h1><?php echo "hola mundo" ?></h1>
+</div>
+
+
+</body>
+</html>
